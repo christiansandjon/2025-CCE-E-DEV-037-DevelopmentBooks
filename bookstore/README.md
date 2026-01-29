1484a70704e8081bcc23134b8ec9fd1289575904
christian s.m <christian.sandjongmotio@hr-rail.be>
2026-01-29 12:29:12 +0100

    Initial project setup

---
+++ bookstore/README.md
@@ -0,0 +1,28 @@
+# Development Books Price Calculator
+
+TDD Kata for BNP Paribas Fortis selection process.
+
+## Problem
+
+Calculate the price of a basket of software development books with discounts based on book diversity.
+
+## Rules
+
+- Base price: 50 EUR per book
+- Discounts on different books:
+  - 2 different: 5%
+  - 3 different: 10%
+  - 4 different: 20%
+  - 5 different: 25%
+
+## Build
+
+```bash
+mvn clean install
+```
+
+## Test
+
+```bash
+mvn test
+```
---
+++ bookstore/src/main/java/be/bnppf/bookstore/Application.java
@@ -0,0 +1,19 @@
+package be.bnppf.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot Application for Development Books Price Calculator.
+ * 
+ * This is a TDD Kata implementation for BNP Paribas Fortis selection process.
+ * 
+ * @author Christian SANDJONG MOTIO
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
