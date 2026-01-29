2c3eecf75b5ed231754d5aeb78f9da65c00d1e36
christian s.m <christian.sandjongmotio@hr-rail.be>
2026-01-29 13:17:52 +0100

    Add first test for empty basket (RED - does not compile yet)

---
+++ bookstore/src/test/java/be/bnppf/bookstore/service/PriceCalculatorTest.java
@@ -0,0 +1,21 @@
+package be.bnppf.bookstore.service;
+
+import be.bnppf.bookstore.model.ShoppingBasket;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PriceCalculatorTest {
+
+    @Test
+    public void emptyBasket_shouldCostZero() {
+        // Given
+        ShoppingBasket basket = new ShoppingBasket();
+        PriceCalculator calculator = new PriceCalculator();
+        
+        // When
+        double price = calculator.calculate(basket);
+        
+        // Then
+        assertEquals(0.0, price);
+    }
+}
