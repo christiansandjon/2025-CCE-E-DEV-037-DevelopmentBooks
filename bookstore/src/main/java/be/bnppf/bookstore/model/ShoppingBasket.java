248fed0735fdbc7c55e26171730d7231155a1633
christian s.m <christian.sandjongmotio@hr-rail.be>
2026-01-29 13:09:33 +0100

    Add ShoppingBasket class with basic methods

---
+++ bookstore/src/main/java/be/bnppf/bookstore/model/ShoppingBasket.java
@@ -0,0 +1,26 @@
+package be.bnppf.bookstore.model;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingBasket {
+    
+    private List<Book> books;
+    
+    public ShoppingBasket() {
+        this.books = new ArrayList<>();
+    }
+    
+    public void add(Book book) {
+        books.add(book);
+    }
+    
+    public int size() {
+        return books.size();
+    }
+    
+    public boolean isEmpty() {
+        return books.isEmpty();
+    }
+}
